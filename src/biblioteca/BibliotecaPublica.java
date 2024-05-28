7f9be1d032fbbada9e9788baf4e5562ddb51c40b
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-05-28 19:48:10 +0000

    mensaje

--- src/biblioteca/BibliotecaPublica.java
+++ src/biblioteca/BibliotecaPublica.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-//practicando comandos2
+//practicando comandos2 remoto
 public class BibliotecaPublica {
 
 	private final static int AFORO_MAXIMO = 1000;
